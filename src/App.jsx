43d57591466508167a8e5bc3c0f0f4424538c734
jvaadu <jv@aadu.agency>
2024-12-28 18:49:53 +0530

    feat: Redirection to freeSheetsAI addon

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
+import React , { useEffect } from "react";
 import LandingPage from "./Components/LandingPage/LandingPage"
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom"
 import PrivacyPolicy from "./Components/PrivacyPolicy/PrivacyPolicy"
 import TermsOfService from "./Components/TOS/TOS"
 import HowTo from "./Components/HowTo/HowTo"
+
+function RedirectToExternal() {
+  useEffect(() => {
+    window.location.href = "https://workspace.google.com/marketplace/app/freesheetsai/351439192813"; // Replace with your desired URL
+  }, []);
+
+  return null; // This component doesn't render anything
+}
 
 function App() {
 
@@ -14,6 +23,7 @@ function App() {
             <Route path="/privacy-policy/" element={<PrivacyPolicy />} />
             <Route path="/terms-and-conditions/" element={<TermsOfService />} />
             <Route path="/how-to/" element={<HowTo />} />
+            <Route path="/app/" element={<RedirectToExternal />} />
           </Routes>
         </Router>
       </div>
