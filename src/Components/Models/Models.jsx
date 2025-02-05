7bf392860ad58acfa16060f90028db4e36e214ac
jvaadu <jv@aadu.agency>
2025-02-05 21:32:28 +0530

    added openai models

--- src/Components/Models/Models.jsx
+++ src/Components/Models/Models.jsx
@@ -7,50 +7,134 @@ export default function ModelsTable() {
       "url": "https://api.groq.com/openai/v1/chat/completions",
       "platform": "groq",
       "developers": "google",
-      "pro-version": true,
+      "pro-version": false
     },
     "2": {
       "model-id": "llama-3.3-70b-versatile",
       "url": "https://api.groq.com/openai/v1/chat/completions",
       "platform": "groq",
       "developers": "meta",
-      "pro-version": false,
+      "pro-version": false
     },
     "3": {
       "model-id": "llama-3.1-8b-instant",
       "url": "https://api.groq.com/openai/v1/chat/completions",
       "platform": "groq",
       "developers": "meta",
-      "pro-version": false,
+      "pro-version": false
     },
     "4": {
       "model-id": "llama-guard-3-8b",
       "url": "https://api.groq.com/openai/v1/chat/completions",
       "platform": "groq",
       "developers": "meta",
-      "pro-version": false,
+      "pro-version": false
     },
     "5": {
       "model-id": "llama3-70b-8192",
       "url": "https://api.groq.com/openai/v1/chat/completions",
       "platform": "groq",
       "developers": "meta",
-      "pro-version": false,
+      "pro-version": false
     },
     "6": {
       "model-id": "llama3-8b-8192",
       "url": "https://api.groq.com/openai/v1/chat/completions",
       "platform": "groq",
       "developers": "meta",
-      "pro-version": false,
+      "pro-version": false
     },
     "7": {
       "model-id": "mixtral-8x7b-32768",
       "url": "https://api.groq.com/openai/v1/chat/completions",
       "platform": "groq",
       "developers": "mistral",
-      "pro-version": false,
+      "pro-version": false
     },
+    "8": {
+      "model-id": "gpt-4o-mini",
+      "url": "https://api.openai.com/v1/chat/completions",
+      "platform": "openai",
+      "developers": "openai",
+      "pro-version": true
+    },
+    "9": {
+      "model-id": "gpt-4o-mini-2024-07-18",
+      "url": "https://api.openai.com/v1/chat/completions",
+      "platform": "openai",
+      "developers": "openai",
+      "pro-version": true
+    },
+    "10": {
+      "model-id": "gpt-3.5-turbo-1106",
+      "url": "https://api.openai.com/v1/chat/completions",
+      "platform": "openai",
+      "developers": "openai",
+      "pro-version": true
+    },
+    "11": {
+      "model-id": "gpt-3.5-turbo-instruct-0914",
+      "url": "https://api.openai.com/v1/chat/completions",
+      "platform": "openai",
+      "developers": "openai",
+      "pro-version": true
+    },
+    "12": {
+      "model-id": "gpt-3.5-turbo-0125",
+      "url": "https://api.openai.com/v1/chat/completions",
+      "platform": "openai",
+      "developers": "openai",
+      "pro-version": true
+    },
+    "13": {
+      "model-id": "gpt-3.5-turbo",
+      "url": "https://api.openai.com/v1/chat/completions",
+      "platform": "openai",
+      "developers": "openai",
+      "pro-version": true
+    },
+    "14": {
+      "model-id": "davinci-002",
+      "url": "https://api.openai.com/v1/chat/completions",
+      "platform": "openai",
+      "developers": "openai",
+      "pro-version": true
+    },
+    "15": {
+      "model-id": "babbage-002",
+      "url": "https://api.openai.com/v1/chat/completions",
+      "platform": "openai",
+      "developers": "openai",
+      "pro-version": true
+    },
+    "16": {
+      "model-id": "o1-mini-2024-09-12",
+      "url": "https://api.openai.com/v1/chat/completions",
+      "platform": "openai",
+      "developers": "openai",
+      "pro-version": true
+    },
+    "17": {
+      "model-id": "o1-preview-2024-09-12",
+      "url": "https://api.openai.com/v1/chat/completions",
+      "platform": "openai",
+      "developers": "openai",
+      "pro-version": true
+    },
+    "18": {
+      "model-id": "o1-mini",
+      "url": "https://api.openai.com/v1/chat/completions",
+      "platform": "openai",
+      "developers": "openai",
+      "pro-version": true
+    },
+    "19": {
+      "model-id": "o1-preview",
+      "url": "https://api.openai.com/v1/chat/completions",
+      "platform": "openai",
+      "developers": "openai",
+      "pro-version": true
+    }
   };
 
   return (
